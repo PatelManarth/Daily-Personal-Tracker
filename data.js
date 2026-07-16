534b5918b40ff715a9fe56b0a4e44d0012e48583
Manarth Patel <51196632+PatelManarth@users.noreply.github.com>
2026-07-16 19:07:32 -0400

    Add recipe data module aggregator

---
+++ data.js
@@ -0,0 +1,5 @@
+import breakfast from './data-breakfast.js';
+import lunch from './data-lunch.js';
+import snacks from './data-snacks.js';
+export * from './data-core.js';
+export const DEFAULT_RECIPES=[...breakfast,...lunch,...snacks];
